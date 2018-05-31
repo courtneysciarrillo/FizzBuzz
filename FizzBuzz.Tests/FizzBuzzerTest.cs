e196fdfc86acbad8d62dce0de97edbc2cc06def6
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-05-31 12:03:03 -0400

    Added Tests for 3, 5, mul 3 and mul 5

--- FizzBuzz.Tests/FizzBuzzerTest.cs
+++ FizzBuzz.Tests/FizzBuzzerTest.cs
@@ -19,5 +19,65 @@ namespace FizzBuzz.Tests
 
             Assert.That(result, Is.EqualTo("1"));
         }
+
+        [Test]
+        public void Get_Returns_2_for_2()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(2);
+
+            Assert.That(result, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void Get_Returns_Fizz_for_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void Get_Returns_Fizz_for_Mul_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(6);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void Get_Returns_4_for_4()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(4);
+
+            Assert.That(result, Is.EqualTo("4"));
+        }
+
+        [Test]
+        public void Get_Returns_Buzz_for_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void Get_Returns_Buzz_for_Mul_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(10);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
     }
 }
--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -10,7 +10,25 @@ namespace FizzBuzz
     {
         public string Get(int num)
         {
-            return "1";
+            if (IsMultipleOf3(num))
+            {
+                return "Fizz";
+            }
+            if (IsMultipleOf5(num))
+            {
+                return "Buzz";
+            }
+            return num.ToString();
+        }
+
+        private bool IsMultipleOf3(int numberToCheck)
+        {
+            return numberToCheck % 3 == 0;
+        }
+
+        private bool IsMultipleOf5(int numberToCheck)
+        {
+            return numberToCheck % 5 == 0;
         }
     }
 }
